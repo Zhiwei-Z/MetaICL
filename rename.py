c2a9556e1b27eda9e3ed6479b0a04cb4e788decd
ZhiweiZhang <zhiweizhang2012@gmail.com>
2024-04-22 21:26:43 -0700

    rename ? files

--- rename.py
+++ rename.py
@@ -6,5 +6,8 @@ for root, dirs, files in os.walk("config"):
     for f in files:
         if ":" in f:
             print("renaming from ", join(root, f), " to ", join(root, f.replace(":", "_")))
-            os.rename(join(root, f), join(root, f.replace(":", "_")))
+            # os.rename(join(root, f), join(root, f.replace(":", "_")))
+        if "?" in f:
+            print("renaming from ", join(root, f), " to ", join(root, f.replace("?", "_")))
+            # os.rename(join(root, f), join(root, f.replace("?", "_")))
     # print(root, dirs, files)
