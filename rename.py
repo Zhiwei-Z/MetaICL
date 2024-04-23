7526884f7504a3b2ef7b85f171defc3d77c3155f
ZhiweiZhang <zhiweizhang2012@gmail.com>
2024-04-22 21:22:52 -0700

    rename files

---
+++ rename.py
@@ -0,0 +1,10 @@
+import os
+
+join = os.path.join
+
+for root, dirs, files in os.walk("config"):
+    for f in files:
+        if ":" in f:
+            print("renaming from ", join(root, f), " to ", join(root, f.replace(":", "_")))
+            os.rename(join(root, f), join(root, f.replace(":", "_")))
+    # print(root, dirs, files)
